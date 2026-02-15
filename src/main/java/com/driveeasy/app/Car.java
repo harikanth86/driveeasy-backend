3e7487d7d3276a28f73d142b9d33ccf3c5ad0d39
Harikanth Moreddy <harikanth820@gmail.com>
2026-02-15 13:04:07 +0530

    Initial Spring Boot backend

--- src/main/java/com/driveeasy/app/Car.java
+++ src/main/java/com/driveeasy/app/Car.java
@@ -2,11 +2,35 @@
 package com.driveeasy.app;
 import jakarta.persistence.*;
 import lombok.Data;
-@Entity @Data
+@Entity
 public class Car {
  @Id @GeneratedValue
  private Long id;
  private String name;
  private double price;
  private boolean available=true;
+ public Long getId() {
+	return id;
+ }
+ public void setId(Long id) {
+	this.id = id;
+ }
+ public String getName() {
+	return name;
+ }
+ public void setName(String name) {
+	this.name = name;
+ }
+ public double getPrice() {
+	return price;
+ }
+ public void setPrice(double price) {
+	this.price = price;
+ }
+ public boolean isAvailable() {
+	return available;
+ }
+ public void setAvailable(boolean available) {
+	this.available = available;
+ }
 }
